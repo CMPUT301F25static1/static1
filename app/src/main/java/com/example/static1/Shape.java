a126881af21a9e1f018983eb85a6b1ed8d32f93f
XY-M4 <zaralily246321@gmail.com>
2025-09-24 19:12:00 -0600

    violet

--- app/src/main/java/com/example/static1/Shape.java
+++ app/src/main/java/com/example/static1/Shape.java
@@ -3,7 +3,7 @@ package com.example.static1;
 public abstract class Shape {
     private int x;
     private int y;
-    String color = "blue";
+    String color = "violet";
 
     public Shape(int x, int y) {
         this.x = x;
