104c700c6f2c957ff6a04bf5c50bbd8604e28f99
Caleb Marquart <84657308+calebmarquart@users.noreply.github.com>
2025-09-25 11:32:15 -0600

    Add new color variable 'color2' to Shape class

--- app/src/main/java/com/example/static1/Shape.java
+++ app/src/main/java/com/example/static1/Shape.java
@@ -5,6 +5,7 @@ public abstract class Shape {
     private int y;
     String color = "blue";
     String color1 = "indigo";
+    String color2 = "pink";
 
     public Shape(int x, int y) {
         this.x = x;
