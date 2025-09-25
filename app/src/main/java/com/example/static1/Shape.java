042010683308804eb0014c9617f49cfcfb8c4d03
Akhil Vishnubhotla <akhilvishnubhotla@Akhils-MacBook-Air-2.local>
2025-09-24 21:41:46 -0600

    Local: add color field to Shape

--- app/src/main/java/com/example/static1/Shape.java
+++ app/src/main/java/com/example/static1/Shape.java
@@ -3,8 +3,7 @@ package com.example.static1;
 public abstract class Shape {
     private int x;
     private int y;
-    String color = "blue";
-    String color1 = "indigo";
+    protected String color = "blue";   
 
     public Shape(int x, int y) {
         this.x = x;
